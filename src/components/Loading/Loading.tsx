73c135a28d039996276008ab38176ad124f67a66
William G <william.godoy53@gmail.com>
2020-05-31 19:58:52 -0300

    test(loading): unit test

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -3,11 +3,13 @@ import { BarLoader } from 'react-spinners';
 
 import LoadingWrapperStyled from './LoadingWrapperStyled';
 import { TLoadingProps } from './types';
+import { Box } from 'reflexbox';
 
 const Loading: FC<TLoadingProps> = ({ full }) =>
   full ? (
     <LoadingWrapperStyled
       alignItems="center"
+      data-testid="full"
       display="flex"
       justifyContent="center"
       height="100%"
@@ -15,7 +17,9 @@ const Loading: FC<TLoadingProps> = ({ full }) =>
       <BarLoader color="#fff" />
     </LoadingWrapperStyled>
   ) : (
-    <BarLoader />
+    <Box data-testid="bar">
+      <BarLoader />
+    </Box>
   );
 
 export default Loading;
