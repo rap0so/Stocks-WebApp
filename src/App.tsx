e664dc4c3a2d74773446fb1a2621819d69a2fa2f
William G <william.godoy53@gmail.com>
2020-05-30 22:17:02 -0300

    style(global-style): remove old code + invoke on app

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,11 @@ import { ThemeProvider } from 'styled-components';
 
 import Loading from 'components/Loading';
 import theme from 'providers/theme';
+import GlobalStyle from 'providers/theme/components/globalStyle';
 
 const App = () => (
   <ThemeProvider theme={theme}>
+    <GlobalStyle />
     <Suspense fallback={<Loading full={true} />}>
       <Router>
         <Switch>
